fcf0377744dd5124a8968c6abef58518681615d9
Gerardxx <gergus2017@gmail.com>
2026-03-09 22:48:22 -0500

    Agregando Forms

--- components/sections/ContactSection.tsx
+++ components/sections/ContactSection.tsx
@@ -125,7 +125,6 @@ export function ContactSection() {
           </div>
         </div>
 
-        {/* Card horizontal - Asesoría */}
         <div className="md:col-span-2 p-8 md:p-10 rounded-[2.5rem] md:rounded-[3rem] border border-arena flex flex-col sm:flex-row items-center gap-8 md:gap-10 bg-crema">
           <div className="relative w-28 h-28 md:w-32 md:h-32 rounded-full overflow-hidden border-4 border-white shadow-xl -rotate-3 shrink-0">
             <Image
